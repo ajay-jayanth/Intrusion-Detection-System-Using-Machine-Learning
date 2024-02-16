6e1090b85fbf72ecc7ae42e893f751b6fbc74f2d
Nevin Gilday <nevingilday@gmail.com>
2024-02-15 19:35:00 -0600

    added one small comment

--- ids_exploration/read-network/main.py
+++ ids_exploration/read-network/main.py
@@ -10,7 +10,7 @@ outputs_path = root_path + "\\outputs"
 cfm_path = bin_path + "\\cfm.bat"
 
 subprocess.call(["pktmon", "start", "--capture"])
-time.sleep(10)
+time.sleep(10) #wait and read 10 seconds of internet traffic
 subprocess.call(["pktmon", "stop"])
 subprocess.call(["pktmon", "pcapng", "PktMon.etl", "-o", "inputs\\pktmon.pcap"])
 subprocess.call(["rm", "PktMon.etl"])
