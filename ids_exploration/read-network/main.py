552f575b743429e35f0a2c598c5d69ad918c50e7
Nevin Gilday <nevingilday@gmail.com>
2024-02-15 19:34:00 -0600

    got CICFlowMeter working and available through python

---
+++ ids_exploration/read-network/main.py
@@ -0,0 +1,19 @@
+import subprocess
+import time
+
+#get paths for later
+from pathlib import Path
+root_path = str(Path().absolute())
+bin_path = root_path + "\\CICFlowmeter\\bin"
+inputs_path = root_path + "\\inputs"
+outputs_path = root_path + "\\outputs"
+cfm_path = bin_path + "\\cfm.bat"
+
+subprocess.call(["pktmon", "start", "--capture"])
+time.sleep(10)
+subprocess.call(["pktmon", "stop"])
+subprocess.call(["pktmon", "pcapng", "PktMon.etl", "-o", "inputs\\pktmon.pcap"])
+subprocess.call(["rm", "PktMon.etl"])
+subprocess.call([cfm_path, inputs_path, outputs_path], cwd=bin_path)
+subprocess.call(["rm", "inputs\\pktmon.pcap"])
+
---
+++ ids_exploration/read-network/readme.txt
@@ -0,0 +1,5 @@
+#In order for main.py to work 
+    *You must be on windows, it isn't configured for Linux/MacOS
+    *You have to have installed npcap from: https://npcap.com/dist/npcap-1.79.exe
+
+#Just run main.py from read-network directory and in read-network/output a csv file will be generated
